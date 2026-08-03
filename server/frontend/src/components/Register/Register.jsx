f4e7e5f322e416d8525169b1d45c023678d7169b
An <annh4906@gmail.com>
2026-08-03 10:42:51 +0700

    Update Register.jsx

--- server/frontend/src/components/Register/Register.jsx
+++ server/frontend/src/components/Register/Register.jsx
@@ -1,16 +1,6 @@
-import React from 'react';
-
-const Register = () => {
-    return (
-        <form>
-            <input type="text" name="Username" placeholder="Username" />
-            <input type="text" name="FirstName" placeholder="First Name" />
-            <input type="text" name="LastName" placeholder="Last Name" />
-            <input type="email" name="Email" placeholder="Email" />
-            <input type="password" name="Password" placeholder="Password" />
-            <button type="submit">Register</button>
-        </form>
-    );
-};
-
-export default Register;
+<input type="text" name="username" placeholder="Username" onChange={(e) => setUserName(e.target.value)} required />
+<input type="text" name="firstname" placeholder="First Name" onChange={(e) => setFirstName(e.target.value)} required />
+<input type="text" name="lastname" placeholder="Last Name" onChange={(e) => setLastName(e.target.value)} required />
+<input type="email" name="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} required />
+<input type="password" name="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required />
+<button type="submit" className="btn btn-primary">Register</button>
